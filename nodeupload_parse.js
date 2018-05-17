37d52aa9c9ec11124ffc696f778079971da9960a
Susmit Basu <bosesusmit2006@gmail.com>
2018-05-17 14:07:39 +0800

    Init

---
+++ nodeupload_parse.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = '/tmp/qcal_upload/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  }
+}).listen(9088);
